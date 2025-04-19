a9a77f444622f2ae44fa5fca78873227f0aa314e
TheRealAngelo <160996509+TheRealAngelo@users.noreply.github.com>
2025-04-19 22:20:19 +0800

    Sidebar and newfiles

--- src/student/sidebar.php
+++ src/student/sidebar.php
@@ -1,6 +1,8 @@
 <div class="sidebar">
     <div>
         <ul>
+            <li><a href="announcements.php"><img src="../../assets/icons/icons8-announcement-100.png">Vision Mission</a></li>
+            <li><a href="announcements.php"><img src="../../assets/icons/icons8-announcement-100.png">Dashboard</a></li>
             <li><a href="dashboard.php"><img src="../../assets/icons/icons8-assessment-100.png">View Grades</a></li>
             <li><a href="announcements.php"><img src="../../assets/icons/icons8-announcement-100.png">View Announcements</a></li>
         </ul>
