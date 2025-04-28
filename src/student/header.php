b2dc367336a311b2bc079b7c74a55d81edf93895
Dmaejavu <140609737+Dmaejavu@users.noreply.github.com>
2025-04-28 21:36:17 +0800

    i push

--- src/student/header.php
+++ src/student/header.php
@@ -1,4 +1,4 @@
-<div class="header-left">
+<div class="header-left"> <!--  class="header-left" -->
         <img src="../../assets/images/SVA_logo.png" alt="ADD LOGO HERE">
         <h1>Student Dashboard</h1> <!-- I WANT TO CHANGE h1 but I need to find good font pa, iz too big (thats !what she said)-->
 </div>
