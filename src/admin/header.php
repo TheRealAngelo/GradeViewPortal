021327111036e4e460269d880efd743ba98953cd
TheRealAngelo <160996509+TheRealAngelo@users.noreply.github.com>
2025-05-01 19:53:46 +0800

    Update header.php

--- src/admin/header.php
+++ src/admin/header.php
@@ -1,6 +1,6 @@
 <div class="header-left">
         <img src="../../assets/images/SVA_logo.png" alt="ADD LOGO HERE">
-        <h1>Admin</h1> <!-- I WANT TO CHANGE h1 but I need to find good font pa, iz too big (thats !what she said)-->
+        <h1>SVA Admin</h1> <!-- I WANT TO CHANGE h1 but I need to find good font pa, iz too big (thats !what she said)-->
 </div>
 
 <div class="header-right">
