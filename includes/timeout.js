6348ae02a161ad96bba51518d34f5a1540550fdb
TheRealAngelo <160996509+TheRealAngelo@users.noreply.github.com>
2025-05-04 15:51:16 +0800

    fixes

--- includes/timeout.js
+++ includes/timeout.js
@@ -1,14 +1,14 @@
 let timeout;
 
-    function resetTimeout() {
-        clearTimeout(timeout);
-        timeout = setTimeout(() => {
-            window.location.href = "../src/logout.php";
-        }, 600000); // (milliseconds)
-    }
+function resetTimeout() {
+    clearTimeout(timeout); 
+    timeout = setTimeout(() => {
+        window.location.href = "/SoftwareEngineering2Final/src/logout.php";
+    }, 600000); //(milliseconds)
+}
 
-    
-    window.onload = resetTimeout;
-    document.onmousemove = resetTimeout;
-    document.onkeypress = resetTimeout;
-    document.onscroll = resetTimeout;
+// Reset timer if meron ff
+window.onload = resetTimeout;
+document.onmousemove = resetTimeout;
+document.onkeypress = resetTimeout;
+document.onscroll = resetTimeout;
